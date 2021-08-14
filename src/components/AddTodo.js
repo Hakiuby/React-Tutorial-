b348fd7fdfab4c05dc565bada4c9fc11bf3557d5
Hakiuby <56271025+Hakiuby@users.noreply.github.com>
2021-08-14 21:31:23 +0700

    reat hook

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,36 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
-class AddTodo extends React.Component {
-  state = {
-    title: "",
+function AddTodo (props){
+  const [title,setTitle] = useState(""); 
+
+  const onInputChange = e => { 
+    setTitle(e.target.value)
   };
 
-  onInputChange = e =>{
-      this.setState({
-        title: e.target.value
-      });
-  };
-
-  addTodo = e=> {
-      e.preventDefault(); 
-      this.props.addTodo(this.state.title); 
-      this.setState({
-        title:""
-      });
+  const addTodo = e => { 
+    e.preventDefault(); 
+    props.addTodo(title); 
+    setTitle("")
   }; 
-  render() {
-    return (
-      <form className="form-container" onSubmit={this.addTodo}>
-        <input
-          type="text"
-          placeholder="Add todo ......"
-          className="input-text"
-          value={this.state.title}
-          onChange={this.onInputChange}
-        />
-        <input type="submit" value="Submit" className="input-submit" />
-      </form>
-    );
-  }
+
+  return (
+    <form className="form=container" onSubmit={addTodo}>
+      <input type="text" placeholder="Add Todo" className="input-text" value={title} onChange={onInputChange} />
+      <input type="submit" value="Submit" className="input-submit" />
+    </form>
+  );
 }
 export default AddTodo;
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,81 +1,62 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Header from "./layout/Header";
 import Todos from "./Todos";
 import AddTodo from "./AddTodo";
 // import { v4 as uuid } from "uuid";
 import axios from "axios";
 
-class TodoApp extends React.Component {
-  state = {
-    todos: [],
+function TodoApp(){
+  const[state,setState]=useState({
+    todos:[]
+  });
+  const handleCheckboxChange =id => {
+    setState({
+     todos: state.todos.map(todo => {
+       if(todo.id === id){
+         todo.completed = !todo.completed; 
+       }
+       return todo; 
+     }) 
+    }); 
   };
-  handleCheckboxChange = (id) => {
-    this.setState({
-      todos: this.state.todos.map((todo) => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      }),
+  const deleteTodo = id => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(response=>setState({
+      todos: [
+        ...state.todos.filter(todo=> {
+          return todo.id !== id; 
+        })
+      ]
+    }))
+  }; 
+  const addTodo = title => {
+    const todoData = { 
+      title:title, 
+      completed:false
+    }
+    axios.post("https://jsonplaceholder.typicode.com/todos", todoData)
+    .then(response=>{
+      console.log(response.data)
+      setState({
+        todos:[...state.todos,response.data]
+      })
     });
   };
-  deleteTodo = (id) => {
-    axios
-      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then((response) =>
-        this.setState({
-          todos: [
-            ...this.state.todos.filter((todo) => {
-              return todo.id !== id;
-            }),
-          ],
-        })
-      );
-  };
-  addTodo = (title) => {
-    const todoData = {
-      title: title,
-      completed: false,
-    };
-    axios
-      .post("https://jsonplaceholder.typicode.com/todos", todoData)
-      .then((response) => {
-        console.log(response.data);
-        this.setState({
-          todos: [...this.state.todos, response.data],
-        });
-      });
-  };
-  componentDidMount() {
-    // Tạo Get request để lấy danh sach todos
-    // Cach 1
-    // axios.get("https://jsonplaceholder.typicode.com/todos")
-    // .then(response => console.log(response.data));
+  useEffect(()=>{
+    const config={
+      params:{
+        _limit:5
+      }
+    }
+    axios.get("https://jsonplaceholder.typicode.com/todos", config).then(response=>setState({todos:response.data})); 
+  },[]); 
 
-    // Cach 2
-
-    const config = {
-      params: {
-        _limit: 5,
-      },
-    };
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos", config)
-      .then((response) => this.setState({ todos: response.data }));
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <Header />
-        <AddTodo addTodo={this.addTodo} />
-        <Todos
-          todos={this.state.todos}
-          handleChange={this.handleCheckboxChange}
-          deleteTodo={this.deleteTodo}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <Header/>
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={state.todos} handleChange={handleCheckboxChange} deleteTodo={deleteTodo} />
+    </div>
+  );
 }
-export default TodoApp;
+export default TodoApp; 
